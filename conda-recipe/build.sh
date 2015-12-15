30bb4f54223fbedc88739291098bd5e3f9f36cc3
David Froger <david.froger@inria.fr>
2015-12-15 00:37:25 -0800

    OSX: build with clang libc++

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,11 @@
+export MACOSX_DEPLOYMENT_TARGET=10.7
+
+export CC=clang
+export CXX=clang++
+export CFLAGS="-mmacosx-version-min=10.7"
+export CXXFLAGS="-stdlib=libc++ -mmacosx-version-min=10.7"
+export LDFLAGS="-stdlib=libc++ -mmacosx-version-min=10.7"
+
 cd numini
 ../waf configure \
     --prefix=$PREFIX \
