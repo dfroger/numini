a5d53d8bfd029e9cc3dbc2202553e46253bef94e
David Froger <david.froger@inria.fr>
2015-12-15 17:07:40 +0100

    Fix recipe on GNU/Linux

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,9 +1,8 @@
-export MACOSX_DEPLOYMENT_TARGET=10.7
-
 cd numini
 
 if [ "$(uname)" == "Darwin" ]
 then export CC=clang
+    export MACOSX_DEPLOYMENT_TARGET=10.7
     export CXX=clang++
 
     unset CXXFLAGS
@@ -16,15 +15,18 @@ then export CC=clang
         --cflags='-mmacosx-version-min=10.7 -arch x86_64' \
         --cxxflags='-stdlib=libc++ -mmacosx-version-min=10.7 -arch x86_64' \
         --ldflags="-stdlib=libc++ -mmacosx-version-min=10.7 -arch x86_64"
+    waf build
+    waf install
 else
     waf configure \
         --prefix=$PREFIX \
         --conda
+    waf build
+    waf install
+    mkdir -p $PREFIX/lib
+    ln -s $PREFIX/lib64/libnumini.so $PREFIX/lib/libnumini.so
 fi
 
-waf build
-
-waf install
 
 # waf (from the waf conda package), extract itself in $PREFIX/bin
 rm -rf $PREFIX/bin/.waf-*
